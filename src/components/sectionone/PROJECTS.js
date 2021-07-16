4a007e91148f8b4498376eeb088b809b9e4cc4df
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-07-16 13:32:40 +0200

    section 3

---
+++ src/components/sectionone/PROJECTS.js
@@ -0,0 +1,34 @@
+export const PROJECTS = [
+    {
+        image: '/projects/carepet.png',
+        titlte: 'CarePet-Web',
+        subtitle: 'Web app for the care and monitoring of pets needs.',
+        resume: null,
+        github: 'https://github.com/joselrdg/CarePet-web',
+        demo: null,
+    },
+    {
+        image: '/projects/api.jpg',
+        titlte: 'FCI-Api',
+        subtitle: 'Api that has more than 350 breeds of dogs with more than 25 sections with information in 4 languages.',
+        resume: null,
+        github: 'https://github.com/joselrdg/CarePet-api',
+        demo: null,
+    },
+    {
+        image: '/projects/influencer.jpg',
+        titlte: 'Infect-Me',
+        subtitle: 'Social network to search for contacts with affinity of interests, editable and customizable with multimedia content.',
+        resume: null,
+        github: 'https://github.com/joselrdg/Infect-me',
+        demo: 'https://infectme.herokuapp.com/',
+    },
+    {
+        image: '/projects/canvas.png',
+        titlte: 'GO TO HELL WITH...CANVAS!!!',
+        subtitle: 'Video game made in JavaScript Vanilla.',
+        resume: null,
+        github: 'https://github.com/joselrdg/project-game',
+        demo: 'https://joselrdg.github.io/project-game/',
+    }
+]
--- src/components/sectionone/SectionThree.jsx
+++ src/components/sectionone/SectionThree.jsx
@@ -1,30 +1,54 @@
 import { ParallaxLayer } from "@react-spring/parallax";
 import React, { useEffect, useState } from "react";
 import "./SectionThree.css";
+import { PROJECTS } from "./PROJECTS";
 
-export const SectionThree = ({parallax}) => {
-    console.log(parallax)
-    const [state, setstate] = useState(parallax)
+export const SectionThree = ({ parallax }) => {
+  const [state, setstate] = useState(parallax);
 
-    useEffect(() => {
-      console.log(parallax)
-    }, [parallax])
+  useEffect(() => {
+    console.log(PROJECTS);
+  }, [parallax]);
   return (
     <>
       <ParallaxLayer
         offset={3}
         speed={0.1}
-        style={
-          {
-            // display: "flex",
-            // flexDirection: "row",
-            // alignItems: "center",
-            // justifyContent: "center",
-            color: "red"
-          }
-        }
+        style={{
+          // display: "flex",
+          // flexDirection: "row",
+          // // alignItems: "center",
+          // justifyContent: "center",
+          color: "red",
+          backgroundColor: "#000000",
+        }}
       >
-        <h1>Work</h1>
+        <div className="card-proyects-container">
+          <div>
+          <h1 className="titleMirror proyectsTitle">Proyects</h1>
+          <h1 className="titleMirror proyectsTitle">&</h1>
+          <h1 className="titleMirror proyectsTitle">Demos</h1>
+          </div>
+          <div className="card-proyects-items">
+            {PROJECTS.map((item) => {
+              return (
+                <div>
+                  <img
+                    // onMouseEnter={() => onMouseEnter(true)}
+                    // onMouseLeave={() => onMouseLeave(false)}
+                    src={item.image}
+                    alt="head"
+                    style={{ width: "100px" }}
+                  />
+                  <div className="card-proyects-title">
+                    <h2 className="titleMirror">{item.titlte}</h2>
+                    <p className="titleMirror">{item.subtitle}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </ParallaxLayer>
     </>
   );
