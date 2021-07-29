71937ccb37d434afc1d92a073f3b19725edf5f48
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-07-29 15:55:10 +0200

    config icons  parallax

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,20 +14,19 @@ import { SectionOne } from "./sectionone/SectionOne";
 import { SectionTwo } from "./sectionone/SectionTwo";
 import { SectionThree } from "./sectionone/SectionThree";
 import { Contact } from "./sectionone/Contact";
+import { IconsSection } from "./sectionone/IconsSection";
 
 // const d = document.getElementById("game");
 
 // Fonts: lasenter  clipneon  moon
 
 const url = (name, wrap = false) =>
-  `${
-    wrap ? "url(" : ""
-  }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
-    wrap ? ")" : ""
-  }`;
+  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
+
 
 export const Main = () => {
   const parallax = useRef();
+ 
 
   // const hidecanvas = () => {
   //   d.style.display = d.style.display == "none" ? "block" : "none";
@@ -53,9 +52,13 @@ export const Main = () => {
             backgroundSize: "cover",
           }}
         />
-        <ParallaxLayer offset={0} speed={-0.8}>
-          <SectionOne />
+        <ParallaxLayer
+          offset={0}
+          speed={-0.8}
+        >
+            <SectionOne />
         </ParallaxLayer>
+       
 
         <ParallaxLayer
           offset={3}
@@ -117,149 +120,14 @@ export const Main = () => {
             backgroundColor: "#000000",
           }}
         />
-        <SectionTwo />
+ <SectionTwo />
 
-        <ParallaxLayer
-          offset={3.3}
-          speed={-0.3}
-          style={{ pointerEvents: "none" }}
-        >
-          <i
-            className="fas fa-code icons_sectionthree"
-            style={{
-              fontSize: "80px",
-              display: "block",
-              width: "100px",
-              marginLeft: "70%",
-            }}
-          ></i>
-        </ParallaxLayer>
+ <IconsSection/>
 
-        <ParallaxLayer offset={3} speed={0.8} style={{ opacity: 0.1 }}>
-          <i
-            className="fab fa-html5 icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "55%",
-            }}
-          ></i>
-          <i
-            className="fab fa-html5 icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "15%",
-            }}
-          ></i>
-          <i
-            className="fab fa-npm icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "15%",
-            }}
-          ></i>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={3.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <i
-            className="fab fa-react icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "70%",
-            }}
-          ></i>
-          <i
-            className="fab fa-react icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "40%",
-            }}
-          ></i>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={3} speed={0.2} style={{ opacity: 0.2 }}>
-          <i
-            className="fab fa-angular icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "10%",
-            }}
-          ></i>
-          <i
-            className="fab fa-angular icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "75%",
-            }}
-          ></i>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={3.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <i
-            className="fab fa-node-js icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "60%",
-            }}
-          />
-          <i
-            className="fab fa-node-js icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "30%",
-            }}
-          />
-          <i
-            className="fab fa-node-js icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "80%",
-            }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={3.8} speed={0.4} style={{ opacity: 0.6 }}>
-          <i
-            className="fab fa-css3-alt icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "5%",
-            }}
-          />
-          <i
-            className="fab fa-css3-alt icons_sectionthree"
-            style={{
-              fontSize: "100px",
-              display: "block",
-              width: "100px",
-              marginLeft: "75%",
-            }}
-          />
-        </ParallaxLayer>
-
+       
         <SectionThree />
 
+       
         <Contact />
       </Parallax>
     </div>
---
+++ src/components/sectionone/IconsSection.jsx
@@ -0,0 +1,60 @@
+import { ParallaxLayer } from "@react-spring/parallax";
+import React from "react";
+import "./IconsSection.css";
+
+const icons = ["html5", "npm", "react", "angular", "sass", "yarn", "node-js", "js-square"];
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const IconsSection = () => {
+  const inconsRand = () => {
+    const items = icons.map((e, i) => {
+      const r = randomInt(30, 150);
+      const opacity = randomInt(1, 10);
+      const margin = randomInt(10, 90);
+      return (
+        <i
+          key={i}
+          className={`fab fa-${e} icons_sectionthree`}
+          style={{
+            fontSize: `${r}px`,
+            opacity: `0.${opacity}`,
+            display: "block",
+            width: `${r}px`,
+            marginLeft: `${margin}%`,
+          }}
+        />
+      );
+    });
+    return items;
+  };
+  return (
+    <>
+      {/* <ParallaxLayer offset={3.3} speed={-0.3} style={{ pointerEvents: 'none', opacity: 0.7 }}>
+          <i className="fas fa-code icons_sectionthree" style={{ fontSize: '80px', display: 'block', width: '100px', marginLeft: '70%'}} ></i>
+        </ParallaxLayer> */}
+
+      <ParallaxLayer offset={3} speed={0.8}>
+        {inconsRand()}
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={3.75} speed={0.5}>
+        {inconsRand()}
+      </ParallaxLayer>
+
+      {/* <ParallaxLayer offset={3} speed={0.2}>
+        {inconsRand()}
+      </ParallaxLayer> */}
+
+      {/* <ParallaxLayer offset={3.6} speed={-0.1}>
+        {inconsRand()}
+      </ParallaxLayer> */}
+
+      {/* <ParallaxLayer offset={3.8} speed={0.4}>
+        {inconsRand()}
+      </ParallaxLayer> */}
+    </>
+  );
+};
