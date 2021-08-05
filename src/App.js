3ee2b0f81ad587ec4a2904f321032111efbef119
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-08-05 11:58:44 +0200

    config ip

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import { HandleClient } from './scriptclient/HandleClient';
 
 function App() {
 
- const onUnload = e => { // the method that will be used for both add and remove event
-    e.preventDefault();
-    e.returnValue = '';
- }
+//  const onUnload = e => { // the method that will be used for both add and remove event
+//     e.preventDefault();
+//     e.returnValue = '';
+//  }
 
-HandleClient()
+// HandleClient()
 
-useEffect(() => {
-  window.addEventListener("beforeunload", onUnload);
-  return () => {
-    window.removeEventListener("beforeunload", onUnload);
-  }
-}, [])
+// useEffect(() => {
+//   window.addEventListener("beforeunload", onUnload);
+//   return () => {
+//     window.removeEventListener("beforeunload", onUnload);
+//   }
+// }, [])
 
   return (
 
