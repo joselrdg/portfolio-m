77e643c7d98aee7f4a6ad6f549461a4080843346
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-08-05 22:48:26 +0200

    up client

--- src/scriptclient/HandleClient.jsx
+++ src/scriptclient/HandleClient.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-import { postClientConnected } from "../services/ClientService";
+import {
+  postClientConnected,
+  upClientConnected,
+} from "../services/ClientService";
 // import { Client } from "./Client";
 import { GetIpify } from "./GetIpify";
 import { GetUserIp } from "./GetUserIp";
@@ -8,43 +11,77 @@ import { ScriptClient as GetGeolocation } from "./GetGeolocation";
 export const HandleClient = () => {
   const [stateId, setstateId] = useState();
   // const dataclient = Client();
-  const refclientconenected = useRef({
-    star: false
-  });
+  const refclientconenected = useRef(false);
+  const geolocationRef = useRef(false);
+  const ipRef = useRef(false);
+  const ipifyRef = useRef(false);
 
   const geolocation = GetGeolocation();
   const ip = GetUserIp();
   const ipify = GetIpify();
-  const startdate = new Date();
+  // const startdate = new Date();
 
   useEffect(() => {
-    if (!refclientconenected.current.start) {
+    if (!refclientconenected.current) {
+      refclientconenected.current = true;
       postClientConnected({ startdate: new Date() })
-      .then((response) => {
-        refclientconenected.current.star = true;
-        const id = response.data.id
-        setstateId(id);
-      })
-      .catch((error) => {
-        // console.log(error);
-      });
-
-      if (ipify !== null){
-        // updapteClientConnected()
-      }
-
-      // if (ip != null && ipify != null && geolocation != null) {
-      //   console.log("use effect todos pasaron", geolocation, ip, ipify);
-      //   refclientconenected.current = true;
-      //   postClientConnected({ startdate, geolocation, ip, ipify })
-      //     .then((response) => {
-      //       setstateId(response.data.id);
-      //     })
-      //     .catch((error) => {
-      //       // console.log(error);
-      //     });
-      // }
+        .then((response) => {
+          const id = response.data.id;
+          setstateId(id);
+        })
+        .catch((error) => {
+          refclientconenected.current = false;
+          // console.log(error);
+        });
     }
 
-  }, [ipify]);
+    if (refclientconenected.current === true) {
+      if (geolocation !== null && !geolocationRef.current) {
+        upClientConnected({ id: stateId, data: { ipify, ip, geolocation } })
+          .then((response) => {
+            geolocationRef.current = true;
+            console.log(response);
+          })
+          .catch((error) => {
+            geolocationRef.current = false;
+            console.log(error);
+          });
+      }
+      if (ip !== null && !ipRef.current) {
+        upClientConnected({ id: stateId, data: { ipify, ip, geolocation } })
+          .then((response) => {
+            ipRef.current = true;
+            console.log(response);
+          })
+          .catch((error) => {
+            ipRef.current = false;
+            console.log(error);
+          });
+      }
+      if (ipify !== null && !ipifyRef.current) {
+        upClientConnected({ id: stateId, data: { ipify, ip, geolocation } })
+          .then((response) => {
+            ipifyRef.current = true;
+            console.log(response);
+          })
+          .catch((error) => {
+            ipifyRef.current = false;
+            console.log(error);
+          });
+      }
+    }
+    console.log('refclientconenected.current',refclientconenected.current)
+
+    // if (ip != null && ipify != null && geolocation != null) {
+    //   console.log("use effect todos pasaron", geolocation, ip, ipify);
+    //   refclientconenected.current = true;
+    //   postClientConnected({ startdate, geolocation, ip, ipify })
+    //     .then((response) => {
+    //       setstateId(response.data.id);
+    //     })
+    //     .catch((error) => {
+    //       // console.log(error);
+    //     });
+    // }
+  }, [geolocation, ip, ipify]);
 };
--- src/services/ClientService.js
+++ src/services/ClientService.js
@@ -2,12 +2,13 @@ import { create } from "./BaseService";
 
 const http = create()
 
-export const createClientConnected = (body) => {
-    return http.post(`create/client/connected`, body)
-}
 
 export const postClientConnected = (body) => {
     return http.post(`/client`, body)
+}
+
+export const upClientConnected = (body) => {
+    return http.post(`/uclient`, body)
 }
 
 export const postClientDisconnected = (body) => {
