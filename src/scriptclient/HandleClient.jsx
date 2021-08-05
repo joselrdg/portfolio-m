7f580021c240ade8a38bec1f23f2d177ba110987
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-08-05 17:57:53 +0200

    client post

--- src/scriptclient/HandleClient.jsx
+++ src/scriptclient/HandleClient.jsx
@@ -1,30 +1,53 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { postClientConnected } from "../services/ClientService";
 import { Client } from "./Client";
+import { GetIpify } from "./GetIpify";
+import { GetUserIp } from "./GetUserIp";
+import { ScriptClient as GetGeolocation } from "./GetGeolocation";
 
 export const HandleClient = () => {
-  const dataclient = Client();
-  const ref = useRef({
-    clientconenected: false,
-  });
+  const [stateId, setstateId] = useState()
+  // const dataclient = Client();
+  const refclientconenected = useRef(false)
+
+  const geolocation = GetGeolocation();
+  const ip = GetUserIp();
+  const ipify = GetIpify();
+  const startdate = new Date();
 
   useEffect(() => {
-    if (
-      dataclient.geolocation !== null &&
-      dataclient.ip !== null &&
-      dataclient.ipify !== null &&
-      !ref.current.clientconenected
-    ) {
-      ref.current.clientconenected = true;
-      postClientConnected(dataclient)
-        .then((response) => {
-          // console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }    
-}, [dataclient])
+    // if (
+    //   !refclientconenected.current.clientconenected
+    // ) {
+    //   refclientconenected.current.clientconenected = true;
+    //   postClientConnected({ startdate })
+    //     .then((response) => {
+    //       console.log(response);
+    //       setstateId(response.data.id);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+    // }
+    if (ip != null && ipify != null && geolocation != null && !refclientconenected.current) {
+      refclientconenected.current = true;
+      postClientConnected({ startdate, geolocation, ip, ipify})
+      .then((response) => {
+        console.log(response);
+        setstateId(response.data.id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+    console.log('useEffect', stateId);
+    console.log('geolocation', geolocation)
+    console.log('ip', ip)
+    console.log('ipify', ipify)
+  
+    
+  }, [startdate, geolocation, ip, ipify])
+
 
 
 
