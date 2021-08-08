733e62a7ef168434ee505b3bd66b14f8d1ec8046
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-08-09 00:36:06 +0200

    table client

--- src/dashboard/DashboardScreen.jsx
+++ src/dashboard/DashboardScreen.jsx
@@ -10,34 +10,60 @@ export const DashboardScreen = () => {
     });
   }, []);
 
-  
-  if (!data) {
-      return <p>Loading...</p>;
+  const condate = (e) => {
+    const d = new Date(e);
+    return (
+      <td>
+        {d.toLocaleDateString()} - {d.toLocaleTimeString()}
+      </td>
+    );
+  };
+
+  const error = (e) => {
+    console.log(e);
+    if (e === undefined) {
+      return <td>undefined</td>;
+    } else if (!e.error || e.error === null) {
+      return (
+        <td>
+          {e.position.latitude}/{e.position.longitude}
+        </td>
+      );
+    } else {
+      return <td>{e.error}</td>;
     }
-    console.log(data[data.length-1])
+  };
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+  console.log(data[data.length - 5]);
 
   return (
     <div style={{ backgroundColor: "white" }}>
-        <table id="customers">
-          <tr>
-            <th>Date</th>
-            <th>Ipify</th>
-            <th>Ip</th>
-            <th>Screen</th>
-            <th>Geolocation</th>
-          </tr>
-      {data.map((e) => {
-          console.log(e.ipify && e.ipify.ip)
+      <table id="customers">
+        <tr>
+          <th>Date</th>
+          <th>Ipify</th>
+          <th>Ip</th>
+          <th>Screen</th>
+          <th>Geolocation</th>
+        </tr>
+        {data.map((e) => {
+          console.log(e.ipify && e.ipify.ip);
           return (
-
-          <tr>
-            <td>{e.startdate}</td>
-            <td>{e.ipify && e.ipify.ip}</td>
-            <td>{e.ip && e.ip.ip}</td>
-            <td>W: {e.screen && e.screen.width} / H: {e.screen && e.screen.height}</td>
-            <td>{e.geolocation && e.geolocation.position}</td>
-          </tr>
-      )})}
+            <tr>
+              {e.startdate && condate(e.startdate)}
+              <td>{e.ipify && e.ipify.ip}</td>
+              <td>{e.ip && e.ip.ip}</td>
+              <td>
+                W: {e.screen && e.screen.width} / H:{" "}
+                {e.screen && e.screen.height}
+              </td>
+              {e.geolocation && error(e.geolocation)}
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
